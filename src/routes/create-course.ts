83dca6ecb2a426cd038a3ab7ffe77f2e722a69a6
Vitor Esser <vitoresser10@gmail.com>
2025-09-07 23:55:59 +0200

    feat: added validation and api route docs

---
+++ src/routes/create-course.ts
@@ -0,0 +1,36 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { db } from '../database/client.ts'
+import { courses } from '../database/schema.ts'
+import z from 'zod'
+
+export const createCourseRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post(
+    '/courses',
+    {
+      schema: {
+        tags: ['courses'],
+        summary: 'Create a course',
+        description:
+          'This route receive a title and create a course in the database',
+        body: z.object({
+          title: z.string().min(5, 'Título precisa ter 5 caracteres'),
+        }),
+        response: {
+          201: z
+            .object({ courseId: z.uuid() })
+            .describe('Course created successfully!'),
+        },
+      },
+    },
+    async (request, reply) => {
+      const courseTitle = request.body.title
+
+      const result = await db
+        .insert(courses)
+        .values({ title: courseTitle })
+        .returning()
+
+      return reply.status(201).send({ courseId: result[0].id })
+    }
+  )
+}
